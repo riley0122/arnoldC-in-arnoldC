6f214f9fd80ac71bda857aac61cace64e809fe7b
LucasAmmer <lucas@ammer.nl>
2023-10-15 23:38:44 +0200

    added reading files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 int main(int argc, char *argv[]){
+    if(argc < 2) { cout << "No input file!" << endl; }
+    ifstream inputFile(argv[1]);
+    string line;
+    vector<string> lines;
+    if(inputFile.is_open()){
+        while (getline(inputFile, line, '"'))
+        {
+            lines.push_back(line);
+        }
+    }
+    inputFile.close();
     return 0;
 }
